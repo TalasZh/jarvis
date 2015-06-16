735563a77f49ffbc2584fe656128d97201b19038
dilshat <daliev@critical-factor.com>
2015-06-16 10:36:30 +0600

    http://jira.critical-factor.com/browse/KMS-62 in progress

--- src/main/java/org/safehaus/analysis/ModelMetrics.java
+++ src/main/java/org/safehaus/analysis/ModelMetrics.java
@@ -1,45 +1,55 @@
 package org.safehaus.analysis;
 
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.safehaus.persistence.CassandraConnector;
+
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.safehaus.persistence.CassandraConnector;
-import org.springframework.ui.Model;
-
-import java.io.Serializable;
-import java.text.MessageFormat;
-import java.util.Date;
 
 import static com.datastax.spark.connector.japi.CassandraJavaUtil.javaFunctions;
-import static com.datastax.spark.connector.japi.CassandraJavaUtil.mapColumnTo;
 import static com.datastax.spark.connector.japi.CassandraJavaUtil.mapRowTo;
+
 
 /**
  * Created by neslihan on 12.06.2015.
  */
-public class ModelMetrics {
+public class ModelMetrics
+{
     private transient SparkConf conf;
     private transient JavaSparkContext jsc;
     private static String keyspace = CassandraConnector.getKeyspace();
     private static String table = CassandraConnector.getCaptureTable();
 
-    public ModelMetrics(String sparkMaster, String appName, String cassandraHost){
-        conf = new SparkConf(true).
-                setMaster(sparkMaster).
-                setAppName(appName).
-                set("spark.cassandra.connection.host", cassandraHost);
-        jsc = new JavaSparkContext(conf);
+
+    public ModelMetrics( String sparkMaster, String appName, String cassandraHost )
+    {
+        conf = new SparkConf( true ).
+                                            setMaster( sparkMaster ).
+                                            setAppName( appName ).
+                                            set( "spark.cassandra.connection.host", cassandraHost );
+        jsc = new JavaSparkContext( conf );
     }
 
-    public void getCaptureData(){
-        JavaRDD<CaptureInternal> captureRDD = javaFunctions(jsc).cassandraTable(keyspace, table, mapRowTo(CaptureInternal.class));
+
+    public void getCaptureData()
+    {
+        JavaRDD<CaptureInternal> captureRDD =
+                javaFunctions( jsc ).cassandraTable( keyspace, table, mapRowTo( CaptureInternal.class ) );
     }
 
-    public void collectMetrics(){}
 
-    public void saveMetrics(){}
+    public void collectMetrics() {}
 
-    public class CaptureInternal implements Serializable {
+
+    public void saveMetrics() {}
+
+
+    public class CaptureInternal implements Serializable
+    {
         private Long id;
         private Date created = new Date();
         private String annotationtext;
@@ -48,9 +58,12 @@ public class ModelMetrics {
         private String anchortext;
         private String newcol;
 
-        public CaptureInternal(){}
 
-        public CaptureInternal(Long id, Date created, String annotationtext, String url, String ancestorid, String anchortext, String newcol)
+        public CaptureInternal() {}
+
+
+        public CaptureInternal( Long id, Date created, String annotationtext, String url, String ancestorid,
+                                String anchortext, String newcol )
         {
             this.id = id;
             this.created = created;
@@ -61,65 +74,86 @@ public class ModelMetrics {
             this.newcol = newcol;
         }
 
+
         public Long getId()
         {
             return id;
         }
+
+
         public void setId()
         {
             this.id = id;
         }
 
+
         public Date getCreated()
         {
             return created;
         }
-        public void setCreated(Date created)
+
+
+        public void setCreated( Date created )
         {
             this.created = created;
         }
+
 
         public String getAnnotationText()
         {
             return annotationtext;
         }
-        public void setAnnotationText(String annotationText)
+
+
+        public void setAnnotationText( String annotationText )
         {
             this.annotationtext = annotationText;
         }
+
 
         public String getUrl()
         {
             return url;
         }
-        public void setUrl(String url)
+
+
+        public void setUrl( String url )
         {
             this.url = url;
         }
+
 
         public String getAncestorId()
         {
             return this.ancestorid;
         }
-        public void setAncestorId(String ancestorId)
+
+
+        public void setAncestorId( String ancestorId )
         {
             this.ancestorid = ancestorId;
         }
+
 
         public String getAnchorText()
         {
             return this.anchortext;
         }
-        public void setAnchorText(String anchorText)
+
+
+        public void setAnchorText( String anchorText )
         {
             this.anchortext = anchorText;
         }
+
 
         public String getNewcol()
         {
             return this.newcol;
         }
-        public void setNewCol(String newcol)
+
+
+        public void setNewCol( String newcol )
         {
             this.newcol = newcol;
         }
