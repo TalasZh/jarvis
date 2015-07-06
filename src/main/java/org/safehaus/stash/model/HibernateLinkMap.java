f8d19dd32f126770343b1db6a2e86014a5784c49
neslihan <nbulut@critical-factor.com>
2015-07-06 13:08:05 +0300

    minor fix: KMS-81: Enable all required object dumps into cassandra after kundera-mysql migration is completed
    	modified:   src/main/java/org/safehaus/stash/model/HibernateLinkMap.java
    	modified:   src/main/java/org/safehaus/stash/model/HibernateSetMap.java

--- src/main/java/org/safehaus/stash/model/HibernateLinkMap.java
+++ src/main/java/org/safehaus/stash/model/HibernateLinkMap.java
@@ -1,7 +1,42 @@
 package org.safehaus.stash.model;
 
+import com.impetus.kundera.index.IndexCollection;
+
+import javax.persistence.*;
+import java.util.Map;
+
 /**
  * Created by neslihan on 03.07.2015.
  */
+@Entity
+@Table( name = "HIBERNATE_LINK_MAP", schema = "jarvis@cassandra-pu" )
+@IndexCollection( columns = {
+        @com.impetus.kundera.index.Index( name = "id" )})
 public class HibernateLinkMap {
+
+    @Id
+    @TableGenerator( name = "id_gen", allocationSize = 30, initialValue = 100 )
+    @GeneratedValue( generator = "id_gen", strategy = GenerationType.TABLE )
+    private long id;
+
+    @ElementCollection
+    @Column(name = "LINKS_MAP_VALUE")
+    private Map<String,String> linksMap;
+
+    public HibernateLinkMap(Map<String,String> linksMap)
+    {
+        this.linksMap = linksMap;
+    }
+
+    public HibernateLinkMap(){}
+
+    public void setLinksMap(Map<String,String> linksMap)
+    {
+        this.linksMap = linksMap;
+    }
+
+    public Map<String,String> getLinksMap()
+    {
+        return linksMap;
+    }
 }
--- src/main/java/org/safehaus/stash/model/HibernateSetMap.java
+++ src/main/java/org/safehaus/stash/model/HibernateSetMap.java
@@ -1,7 +1,42 @@
 package org.safehaus.stash.model;
 
+import com.impetus.kundera.index.*;
+
+import javax.persistence.*;
+import javax.persistence.Index;
+import java.util.Set;
+
 /**
  * Created by neslihan on 03.07.2015.
  */
+@Entity
+@Table( name = "HIBERNATE_SET_MAP", schema = "jarvis@cassandra-pu" )
+@IndexCollection( columns = {
+        @com.impetus.kundera.index.Index( name = "id" )})
 public class HibernateSetMap {
+
+    @Id
+    @TableGenerator( name = "id_gen", allocationSize = 30, initialValue = 100 )
+    @GeneratedValue( generator = "id_gen", strategy = GenerationType.TABLE )
+    private long id;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    @Column(name = "LINKS_SET_VALUE")
+    private Set<HibernateLinkMap> linksValue;
+
+    public HibernateSetMap(Set<HibernateLinkMap> linksValue) {
+        this.linksValue = linksValue;
+    }
+
+    public HibernateSetMap() {}
+
+    public void setLinksValue(Set<HibernateLinkMap> linksValue)
+    {
+        this.linksValue = linksValue;
+    }
+
+    public Set<HibernateLinkMap> getLinksValue()
+    {
+        return linksValue;
+    }
 }
