96a422ee7dad8287fd9291064ca976461c007b9d
Talas Zholdoshbekov <tjoldoshbekov@critical-factor.com>
2015-07-12 20:51:31 +0600

    staging cleaned up plugin

--- index.js
+++ index.js
@@ -6,9 +6,6 @@
 //   callback(text);
 // }
 
-// exports.dummy = dummy;
-
-var {Cc, Cu, Ci} = require('chrome');
 var { ActionButton } = require('sdk/ui/button/action');
 var tabs = require("sdk/tabs");
 var data = require('sdk/self').data;
@@ -30,10 +27,6 @@ function handleClick(state) {
   tabs.open("http://www.mozilla.org/");
 }
 
-function makeURI(aURL, aOriginCharset, aBaseURI) {
-  var ioService = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
-  return ioService.newURI(aURL, aOriginCharset, aBaseURI);
-}
 
 
 exports.main = function(options) {
@@ -42,23 +35,10 @@ exports.main = function(options) {
 		include: ["*"],
 		//contentStyleFile: [data.url("materialize/custom-materialize.css")],
 		// contentStyle: '@font-face{font-family: "Material-Design-Icons";src: url("'+ data.url("materialize/font/material-design-icons/Material-Design-Icons.woff") + '") format("woff"); font-weight: normal;	font-style: normal;	}',
-		contentScriptWhen: "start",
+		contentScriptWhen: "ready",
 		contentScriptFile: [data.url("jquery-2.1.3.min.js"),
 			data.url("floatingElement.js")],
 		onAttach: function(worker) {
-			
-			// var sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
-			// var ios = Cc['@mozilla.org/network/io-service;1'].getService(Ci.nsIIOService);
-
-			// var css = "@font-face{" +
-			// 	"font-family: 'Material-Design-Icons';" +
-			//     "src:url(" + data.url('materialize/font/material-design-icons/Material-Design-Icons.woff') + ") format('woff')," +
-			//     "}";
-
-			// //var cssEnc = 'data:text/css;base64,' + window.btoa(css);
-			// var cssEnc = encodeURIComponent(css);
-			// var cssUri = makeURI('data:text/css,' + cssEnc);
-			// sss.loadAndRegisterSheet(cssUri, sss.USER_SHEET);
 
 			worker.postMessage({some: "options"});
 			worker.port.on("requestResource", function(resourceName, target){
