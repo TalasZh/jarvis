3a9e8a536f716d6f9ce29d4829ee9a6e475e9d7e
Talas Zholdoshbekov <tjoldoshbekov@critical-factor.com>
2015-07-21 17:15:15 +0600

    added jira jql support per user issue retrieval

--- data/floatingElement.js
+++ data/floatingElement.js
@@ -1,69 +1,99 @@
-
 self.port.emit("requestResource", "mfb/fbButtons.html", "body");
 
-self.port.on("loadResource", function(resource, target) {
-	console.log("resource is loaded");
-	console.log(target);
-	jQuery(target).append(resource);
+self.port.on("loadResource", function (resource, target) {
+    console.log("resource is loaded");
+    console.log(target);
+    jQuery(target).append(resource);
+    var annotatorControl = jQuery("#jarvis-annotation-control");
+    annotatorControl.on("click", function () {
+        annotatorIsOn = !annotatorIsOn;
+        console.log("Annotator status: " + annotatorIsOn);
+        enableAnnotator();
+    });
+
+	var researchList = jQuery("#research-list");
+	researchList.on("click", function() {
+		self.port.emit("getResearchList");
+	});
+    enableAnnotator();
 });
 
-enableAnnotator();
+self.port.on("setResearches", function(researches) {
+	var researchList = jQuery("#researchList");
+	researchList.empty();
+	for(var i = 0; i)
+});
+
+var annotatorIsOn = false;
+
 initializeFloatingMenu();
 
 function enableAnnotator() {
-	var content = jQuery("body").annotator();
-	content.annotator('addPlugin', 'Offline', {
-		online:  function () {
-			jQuery("#status").text("Online");
-		},
-		offline: function () {
-			jQuery("#status").text("Offline");
-		}
-	});
+    //var app = new annotator.App();
+    //console.log(app);
+    //app.include(annotator.ui.main);
+    //app.include(annotator.storage.http);
+    //app.start();
+    //alert("Some message for ya");
 
-	var annotator = content.data('annotator');
 
-	jQuery("#clear-storage").click(function () {
-		if (annotator) {
-			annotator.plugins.Offline.store.clear();
-		}
-	});
+    var content = jQuery("body").annotator({
+        readOnly: annotatorIsOn
+    });
+
+    content.annotator('addPlugin', 'Offline', {
+        online: function () {
+            jQuery("#status").text("Online");
+        },
+        offline: function () {
+            jQuery("#status").text("Offline");
+        }
+    });
+    
+    var annotator = content.data('annotator');
+    
+    jQuery("#clear-storage").click(function () {
+        if (annotator) {
+            annotator.plugins.Offline.store.clear();
+        }
+    });
 }
 
+
 function initializeFloatingMenu() {
-	var panel = jQuery('#panel'),
-		menu = jQuery('#jarvis-menu'),
-		showcode = jQuery('#showcode'),
-		selectFx = jQuery('#selections-fx'),
-		selectPos = jQuery('#selections-pos'),
+    var panel = jQuery('#panel'),
+        menu = jQuery('#jarvis-menu'),
+        showcode = jQuery('#showcode'),
+        selectFx = jQuery('#selections-fx'),
+        selectPos = jQuery('#selections-pos'),
 // demo defaults
-		effect = 'mfb-slidein-spring',
-		pos = 'mfb-component--br';
+        effect = 'mfb-slidein-spring',
+        pos = 'mfb-component--br';
 
-	showcode.on('click', _toggleCode);
-	selectFx.on('change', switchEffect);
-	selectPos.on('change', switchPos);
+    showcode.on('click', _toggleCode);
+    selectFx.on('change', switchEffect);
+    selectPos.on('change', switchPos);
 
-	function _toggleCode() {
-		panel.classList.toggle('viewCode');
-	}
+    function _toggleCode() {
+        panel.classList.toggle('viewCode');
+    }
 
-	function switchEffect(e){
-		effect = this.options[this.selectedIndex].value;
-		renderMenu();
-	}
+    function switchEffect(e) {
+        effect = this.options[this.selectedIndex].value;
+        renderMenu();
+    }
 
-	function switchPos(e){
-		pos = this.options[this.selectedIndex].value;
-		renderMenu();
-	}
+    function switchPos(e) {
+        pos = this.options[this.selectedIndex].value;
+        renderMenu();
+    }
 
-	function renderMenu() {
-		menu.style.display = 'none';
-		// ?:-)
-		setTimeout(function() {
-			menu.style.display = 'block';
-			menu.className = pos + effect;
-		},1);
-	}
+    function renderMenu() {
+        menu.style.display = 'none';
+        // ?:-)
+        setTimeout(function () {
+            menu.style.display = 'block';
+            menu.className = pos + effect;
+        }, 1);
+    }
 }
--- index.js
+++ index.js
@@ -11,47 +11,66 @@ var tabs = require("sdk/tabs");
 var data = require('sdk/self').data;
 var pageMod = require("sdk/page-mod");
 var floatingCtrls = [];
+var JiraApi = require("jira-module").JiraApi;
+var jira = new JiraApi("https://jira.subutai.io", "2", false, false);
+var researches = null;
 
 var button = ActionButton({
-  id: "my-button",
-  label: "Visit Mozilla",
-  icon: {
-    "16": data.url('icon-16.png'),
-    "32": data.url('icon-32.png'),
-    "64": data.url('icon-64.png')
-  },
-  onClick: handleClick
+    id: "jarvis-activator",
+    label: "Enable/Disable Jarvis",
+    icon: {
+        "16": data.url('icon-16.png'),
+        "32": data.url('icon-32.png'),
+        "64": data.url('icon-64.png')
+    },
+    onClick: handleClick
 });
 
+
 function handleClick(state) {
-  tabs.open("http://www.mozilla.org/");
+    tabs.open("http://www.mozilla.org/");
 }
 
-exports.main = function(options) {
+exports.main = function (options) {
 
-	var floatingCtrl = pageMod.PageMod({
-		include: ["*"],
-		contentStyleFile: [data.url("mfb/custom-materialize.css"),
-			data.url("annotator-full.1.2.10/annotator.min.css"),
-			data.url("mfb/mfb.css"),
-			data.url("mfb/index.css")
-		],
-		// contentStyle: '@font-face{font-family: "Material-Design-Icons";src: url("'+ data.url("materialize/font/material-design-icons/Material-Design-Icons.woff") + '") format("woff"); font-weight: normal;	font-style: normal;	}',
-		contentScriptWhen: "ready",
-		contentScriptFile: [data.url("jquery-2.1.3.min.js"),
-			data.url("annotator-full.1.2.10/annotator-full.min.js"),
-			data.url("annotator.offline.min.js"),
-			data.url("mfb/mfb.js"),
-			data.url("mfb/modernizr.touch.js"),
-			data.url("floatingElement.js")],
-		onAttach: function(worker) {
+    var floatingCtrl = pageMod.PageMod({
+        include: ["*"],
+        attachTo: ["top"],
+        contentStyleFile: [data.url("mfb/custom-materialize.css"),
+            //data.url("annotator-full.1.2.10/annotator.min.css"),
+            data.url("mfb/mfb.css"),
+            data.url("mfb/index.css")
+        ],
+        // contentStyle: '@font-face{font-family: "Material-Design-Icons";src: url("'+
+        // data.url("materialize/font/material-design-icons/Material-Design-Icons.woff") + '") format("woff");
+        // font-weight: normal;	font-style: normal;	}',
+        contentScriptWhen: "ready",
+        contentScriptFile: [data.url("jquery-2.1.3.min.js"),
+            //data.url("annotator-full.1.2.10/annotator-full.min.js"),
+            //data.url("annotator.offline.min.js"),
+            data.url("annotator.min.js"),
+            data.url("mfb/mfb.js"),
+            data.url("mfb/modernizr.touch.js"),
+            data.url("floatingElement.js")],
+        onAttach: function (worker) {
 
-			worker.postMessage({some: "options"});
-			worker.port.on("requestResource", function(resourceName, target){
-				console.log(data.url(resourceName));
-				console.log(target);
-				worker.port.emit("loadResource", data.load(resourceName), target);
-			});
-		}
-	});
+            console.log("Worker initialized");
+            worker.postMessage({some: "options"});
+            worker.port.on("requestResource", function (resourceName, target) {
+                console.log(data.url(resourceName));
+                console.log(target);
+                worker.port.emit("loadResource", data.load(resourceName), target);
+            });
+			
+            
+			jira.searchJira('issuetype in (Story) AND resolution = Unresolved AND assignee in (currentUser()) ORDER BY updatedDate DESC', null, function (error, json) {
+				if(error) {
+					console.error("Request completed with errors: " + error);
+				}
+				else {
+					console.log(json);
+				}
+            });
+        }
+    });
 }
