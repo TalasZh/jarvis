9c3e186ed832841853c62d7ada3bf289a7af87a3
Talas Zholdoshbekov <tjoldoshbekov@critical-factor.com>
2015-07-28 11:50:50 +0600

    -m KMS-102 staging issues list presentation

--- data/floatingElement.js
+++ data/floatingElement.js
@@ -1,59 +1,73 @@
-(function(window, document, undefined){
-	var annotatorIsOn = false;
+(function (window, document, undefined) {
+    jQuery("head").append('<link rel="stylesheet" href="resource://jarvis-addon/data/mfb/index.css">');
+    jQuery("head").append('<link rel="stylesheet" href="resource://jarvis-addon/data/mfb/mfb.css">');
+    jQuery("head").append('<link rel="stylesheet" href="resource://jarvis-addon/data/mfb/custom-materialize.css">');
 
-	self.port.emit("requestResource", "mfb/fbButtons.html", "body");
+    var annotatorIsOn = false;
 
-	self.port.on("loadResource", function (resource, target) {
-		console.log("resource is loaded");
-		console.log(target);
-		jQuery(target).append(resource);
-		var annotatorControl = jQuery("#jarvis-annotation-control");
-		annotatorControl.on("click", function () {
-			annotatorIsOn = !annotatorIsOn;
-			console.log("Annotator status: " + annotatorIsOn);
-			//enableAnnotator();
+    self.port.emit("requestResource", "mfb/fbButtons.html", "body");
 
-		});
+    self.port.on("loadResource", function (resource, target) {
+        console.log("resource is loaded");
+        console.log(target);
+        jQuery(target).append(resource);
+        var annotatorControl = jQuery(".mfb-component__button--child.btn-jarvis-annotation-control");
+        annotatorControl.on("click", function () {
+            annotatorIsOn = !annotatorIsOn;
+            console.log("Annotator status: " + annotatorIsOn);
+            //enableAnnotator();
 
-		var researchList = jQuery("#list-researches");
-		researchList.on("click", function () {
-			console.log("research list is clicked");
-			self.port.emit("getResearchList");
-			jQuery("#research-list").slideToggle();
-		});
-		jQuery("#research-list").slideToggle();
-		enableAnnotator();
-	});
+        });
 
-	self.port.on("setResearches", function (researches) {
-		var researchList = jQuery("#research-list");
-		researchList.empty();
-		for (let research of researches) {
-			researchList.append("<dt>" + research.key + "</dt>");
-		}
-	});
+        var researchList = jQuery(".mfb-component__button--child.btn-jarvis-list-researches");
+        researchList.on("click", function () {
+            console.log("research list is clicked");
+            //jQuery(".jarvis-research-list").data("jarvis-list-state", "expand");
+            self.port.emit("getResearchList");
+            //jQuery("#research-list").slideToggle();
+        });
+        //jQuery("#research-list").slideToggle();
+        enableAnnotator();
+    });
+
+    self.port.on("setResearches", function (error, researches) {
+        var researchList = jQuery(".jarvis-research-list dl");
+        researchList.empty();
+        if (error) {
+            //alert("Please make sure that service is working now. ");
+            alert("Error: \n" + error);
+        }
+        else {
+            for (let research of researches) {
+                researchList.append("<dt >" + research.key + "</dt>");
+            }
+            if (researches.length == 0) {
+                alert("No Research issues are pulled...");
+            }
+        }
+    });
 
 
-	function enableAnnotator() {
-		var content = jQuery("body").annotator({
-			readOnly: annotatorIsOn
-		});
+    function enableAnnotator() {
+        var content = jQuery("body").annotator({
+            readOnly: annotatorIsOn
+        });
 
-		content.annotator('addPlugin', 'Offline', {
-			online: function () {
-				jQuery("#status").text("Online");
-			},
-			offline: function () {
-				jQuery("#status").text("Offline");
-			}
-		});
+        content.annotator('addPlugin', 'Offline', {
+            online: function () {
+                jQuery("#status").text("Online");
+            },
+            offline: function () {
+                jQuery("#status").text("Offline");
+            }
+        });
 
-		var annotator = content.data('annotator');
+        var annotator = content.data('annotator');
 
-		jQuery("#clear-storage").click(function () {
-			if (annotator) {
-				annotator.plugins.Offline.store.clear();
-			}
-		});
-	}
+        jQuery("#clear-storage").click(function () {
+            if (annotator) {
+                annotator.plugins.Offline.store.clear();
+            }
+        });
+    }
 })(window, document);
--- index.js
+++ index.js
@@ -10,64 +10,52 @@ var { ActionButton } = require('sdk/ui/button/action');
 var tabs = require("sdk/tabs");
 var data = require('sdk/self').data;
 var pageMod = require("sdk/page-mod");
-var floatingCtrls = [];
-var researches = null;
-var jiraError = null;
-var searchQuery ='issuetype in (Story) AND resolution = Unresolved AND assignee in (currentUser()) ORDER BY updatedDate DESC';
-
-
+var searchQuery = 'resolution = Unresolved AND assignee in (currentUser()) ORDER BY updatedDate DESC';
 var JiraApi = require("jira-module").JiraApi;
-
-var jira = new JiraApi("https://jira.subutai.io", "2", false, false);
-pullJiraIssues();
-
-var button = ActionButton({
-    id: "jarvis-activator",
-    label: "Enable/Disable Jarvis",
-    icon: {
-        "16": data.url('jarvis_logo_16x16_cropped.png'),
-        "32": data.url('jarvis_logo_32x32_cropped.png'),
-        "64": data.url('jarvis_logo_64x64_cropped.png')
-    },
-    onClick: handleClick
-});
-
-
-function handleClick(state) {
-    tabs.open("http://www.mozilla.org/");
-}
-
-function pullJiraIssues() {
-	jira.searchJira(searchQuery, null, function (error, json) {
-		if (error) {
-			jiraError = error;
-			console.error("Request completed with errors: " + error);
-		}
-		else {
-			researches = json.issues;
-			console.log(researches);
-		}
-	});
-}
+var simplePrefs = require("sdk/simple-prefs");
 
 exports.main = function (options) {
 
-    var floatingCtrl = pageMod.PageMod({
+    var floatingCtrls = [];
+    var researches = [];
+    var jiraError = null;
+
+    var jira = new JiraApi(simplePrefs.prefs.jiraHost, "2", false, false);
+	
+	simplePrefs.on("applyChanges", onPrefChange);
+
+    pullResearches(false);
+
+    var button = ActionButton({
+        id: "jarvis-activator",
+        label: "Enable/Disable Jarvis",
+        icon: {
+            "16": data.url('jarvis_logo_16x16_cropped.png'),
+            "32": data.url('jarvis_logo_32x32_cropped.png'),
+            "64": data.url('jarvis_logo_64x64_cropped.png')
+        },
+        onClick: handleClick
+    });
+    
+	var floatingCtrl = pageMod.PageMod({
         include: ["*"],
         attachTo: ["top"],
-        contentStyleFile: [data.url("mfb/custom-materialize.css"),
-            data.url("annotator-full.1.2.10/annotator.min.css"),
-            data.url("mfb/mfb.css"),
-            data.url("mfb/index.css")
+        contentStyleFile: [
+            //data.url("mfb/custom-materialize.css"),
+            //data.url("mfb/mfb.css"),
+            //data.url("mfb/index.css"),
+            data.url("annotator-full.1.2.10/annotator.min.css")
         ],
-        contentScriptWhen: "end",
-        contentScriptFile: [data.url("jquery-2.1.3.min.js"),
+        contentScriptWhen: "ready",
+        contentScriptFile: [
+            data.url("jquery-2.1.3.min.js"),
             data.url("annotator-full.1.2.10/annotator-full.min.js"),
             data.url("annotator.offline.min.js"),
             //data.url("annotator.min.js"),
-            data.url("mfb/mfb.js"),
+            //data.url("mfb/mfb.js"),
             data.url("mfb/modernizr.touch.js"),
-            data.url("floatingElement.js")],
+            data.url("floatingElement.js")
+        ],
         onAttach: function (worker) {
 
             console.log("Worker initialized");
@@ -79,14 +67,86 @@ exports.main = function (options) {
             });
 
             worker.port.on("getResearchList", function () {
-				if (jiraError) {
-					pullJiraIssues();
-				}
-				else if (researches) {
-                    console.log(researches);
-                    worker.port.emit("setResearches", researches);
+                console.log("Jira error: " + jiraError);
+                if (jiraError) {
+                    console.log("Couldn't retrieve issues");
+                    worker.port.emit("setResearches", jiraError, []);
+                    pullResearches(true);
+                }
+                else if (researches) {
+                    console.log("Researches: " + researches);
+                    worker.port.emit("setResearches", null, researches);
                 }
             });
         }
     });
+
+
+    function handleClick(state) {
+        tabs.open("http://www.mozilla.org/");
+    }
+
+    function pullResearches(redirect) {
+		
+		isUserAuth(redirect, pullJiraResearches);
+
+        //jira.getCurrentUser(function(error, responseText) {
+        //    if (error) {
+        //        jiraError = error;
+        //        console.error("User is not authenticated... \n" + error);
+		//		if(redirect) {
+		//			tabs.open(simplePrefs.prefs.jiraHost);
+		//		}
+        //    }
+        //    else {
+        //        console.log(responseText);
+        //        console.log("Pulling researches");
+        //        jira.searchJira(searchQuery, null, function (researchError, json) {
+        //            if (researchError) {
+        //                jiraError = researchError;
+        //                console.error("Request completed with errors: " + researchError);
+        //            }
+        //            else {
+        //                jiraError = null;
+        //                researches = json.issues;
+        //                console.log("Researches: " + researches);
+        //            }
+        //        });
+        //    }
+        //});
+    }
+
+	function pullJiraResearches() {
+		jira.searchJira(searchQuery, null, function (researchError, json) {
+			if (researchError) {
+				jiraError = researchError;
+				console.error("Request completed with errors: " + researchError);
+			}
+			else {
+				jiraError = null;
+				researches = json.issues;
+				console.log("Researches: " + researches);
+			}
+		});
+
+	}
+
+	function isUserAuth(redirect, callback) {
+		jira.getCurrentUser(function(error, responseText){
+			if(error) {
+				jiraError=error;
+				if(redirect){
+					tabs.open(simplePrefs.prefs.jiraHost);
+				}
+			}
+			else {
+				callback();
+			}
+		});
+	}
+ 
+	function onPrefChange() {
+		jira = new JiraApi(simplePrefs.prefs.jiraHost, "2", false, false);
+	}
+
 }
