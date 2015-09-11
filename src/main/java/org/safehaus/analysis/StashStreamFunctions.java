b1e161e010f70f25f0abcda8222531fe2039fed5
neslihan <nbulut@critical-factor.com>
2015-09-11 17:04:57 +0300

    KMS-244 Collaboration metric 	modified:   src/main/java/org/safehaus/analysis/StashStreamFunctions.java 	new file:   src/main/java/org/safehaus/analysis/StashUserCollaborationMetricInfo.java 	modified:   src/main/resources/META-INF/persistence.xml

--- src/main/java/org/safehaus/analysis/StashStreamFunctions.java
+++ src/main/java/org/safehaus/analysis/StashStreamFunctions.java
@@ -15,10 +15,11 @@ import org.apache.spark.streaming.kafka.KafkaUtils;
 import org.safehaus.stash.model.StashMetricIssue;
 import scala.Tuple2;
 import scala.Tuple3;
-
+import com.google.common.base.Optional;
 import java.util.*;
-import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
+import org.safehaus.analysis.StashUserCollaborationMetricInfo.StashUserCollaborationMetricInfoInternal;
+import org.safehaus.analysis.StashUserMetricInfo.StashUserMetricInfoInternal;
 
 import static com.datastax.spark.connector.japi.CassandraJavaUtil.mapToRow;
 import static com.datastax.spark.connector.japi.CassandraStreamingJavaUtil.javaFunctions;
@@ -29,9 +30,54 @@ import static com.datastax.spark.connector.japi.CassandraStreamingJavaUtil.javaF
 public class StashStreamFunctions {
     private static String keyspaceName = new String("jarvis");
     private static String stashMetricTlbName = new String("user_stash_metric_info");
+    private static String stashCollabMetricTlbName = new String("user_collaboration_metric_info");
     private static AtomicReference<Date> mostRecentStashCommitDate = new AtomicReference<>();
 
-    public static Function2<List<Tuple3<Integer, Integer,Integer>>, com.google.common.base.Optional<Tuple3<Integer, Integer,Integer>>, com.google.common.base.Optional<Tuple3<Integer, Integer,Integer>>> STASH_COMMIT_RUNNING_SUM =
+    public static Function2<List<Tuple3<Integer, Integer,String>>, Optional<HashSet<String>>, Optional<HashSet<String>>> STASH_COLLABORATION_PROJECTS_COUNT =
+    new Function2<List<Tuple3<Integer, Integer,String>>, Optional<HashSet<String>>, Optional<HashSet<String>>>() {
+        public Optional<HashSet<String>> call(List<Tuple3<Integer, Integer,String>> values, Optional<HashSet<String>> state) throws Exception {
+            Integer thisMonth = -1;
+            Integer thisYear = -1;
+
+            if (values == null || values.isEmpty()) {
+                return state;
+            } else{
+                Tuple3<Integer, Integer,String> entry = values.get(0);
+                thisMonth = entry._1();
+                thisYear = entry._2();
+            }
+
+            HashSet<String> projectNames = new HashSet<>();
+            if(state.isPresent()){
+                projectNames = state.get();
+            }
+
+
+            for (Tuple3<Integer, Integer,String> value : values) {
+                projectNames.add(value._3());
+            }
+
+            if(mostRecentStashCommitDate.get() != null)
+            {
+                Calendar cal = Calendar.getInstance();
+                cal.setTime( mostRecentStashCommitDate.get() );
+
+                if( cal.get( Calendar.YEAR ) > thisYear || (cal.get(Calendar.YEAR) == thisYear && cal.get(Calendar.MONTH) > thisMonth))
+                {
+                    System.out.println("Returning absent! Project Names: "+thisMonth+"-"+thisYear);
+                    for(String name : projectNames){
+                        System.out.print(name);
+                    }
+                    System.out.println();
+                    return Optional.absent();
+                }
+            }
+
+            return Optional.of(projectNames);
+        }
+    };
+
+    public static Function2<List<Tuple3<Integer, Integer,Integer>>, com.google.common.base.Optional<Tuple3<Integer, Integer,Integer>>,com.google.common.base.Optional<Tuple3<Integer, Integer,Integer>>> STASH_COMMIT_RUNNING_SUM =
             new Function2<List<Tuple3<Integer, Integer,Integer>>, com.google.common.base.Optional<Tuple3<Integer, Integer,Integer>>, com.google.common.base.Optional<Tuple3<Integer, Integer,Integer>>>() {
                 @Override
                 public com.google.common.base.Optional<Tuple3<Integer, Integer,Integer>> call(List<Tuple3<Integer, Integer,Integer>> values, com.google.common.base.Optional<Tuple3<Integer, Integer,Integer>> state) throws Exception {
@@ -58,7 +104,7 @@ public class StashStreamFunctions {
                             if( cal.get( Calendar.YEAR ) > thisYear || (cal.get(Calendar.YEAR) == thisYear && cal.get(Calendar.MONTH) > thisMonth))
                             {
                                 //System.out.println("Returning absent! values: "+thisMonth+"-"+thisYear);
-                                return com.google.common.base.Optional.absent();
+                                return Optional.absent();
                             }
                         }
 
@@ -79,12 +125,85 @@ public class StashStreamFunctions {
 
 
                     newSum = new Tuple3<Integer, Integer, Integer>(allCommitsCnt, thisMonth, thisYear);
-                    return com.google.common.base.Optional.of(newSum);
+                    return Optional.of(newSum);
                 }
             };
 
-    public static void computeStashProductivityMetric(JavaDStream<StashMetricIssue> stashIssuesDStream)
+    public static void computeStashCollabMetric(final JavaDStream<StashMetricIssue> stashIssuesDStream)
     {
+        //author,month,year - month,year,numofDifferentProjects
+        JavaPairDStream<Tuple3<String, Integer, Integer>, HashSet<String>> stashUserMonthlyCollaborationMetrics = stashIssuesDStream.filter(new Function<StashMetricIssue, Boolean>() {
+            @Override
+            public Boolean call(StashMetricIssue stashMetricIssue) throws Exception {
+                return (stashMetricIssue!= null && stashMetricIssue.getAuthor() != null && stashMetricIssue.getAuthor().getName() != null && stashMetricIssue.getProjectName() != null);
+            }
+        }).mapToPair(new PairFunction<StashMetricIssue, Tuple3<String, Integer, Integer>, Tuple3<Integer, Integer, String>>() {
+            @Override
+            public Tuple2<Tuple3<String, Integer, Integer>, Tuple3<Integer, Integer, String>> call(StashMetricIssue stashMetricIssue) throws Exception {
+                Date date = new Date(stashMetricIssue.getAuthorTimestamp());
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(date);
+                Integer month = cal.get(Calendar.MONTH);
+                Integer year = cal.get(Calendar.YEAR);
+                String projectName = stashMetricIssue.getProjectName();
+                String author = stashMetricIssue.getAuthor().getName();
+
+                return new Tuple2<Tuple3<String, Integer, Integer>, Tuple3<Integer, Integer, String>>(new Tuple3<String, Integer, Integer>(author, month, year),
+                        new Tuple3<Integer, Integer, String>(month, year, projectName));
+
+            }
+        }).updateStateByKey(STASH_COLLABORATION_PROJECTS_COUNT);
+
+        stashUserMonthlyCollaborationMetrics.print();
+
+        JavaDStream<StashUserCollaborationMetricInfoInternal> collabMetricsDStream = stashUserMonthlyCollaborationMetrics.filter(new Function<Tuple2<Tuple3<String, Integer, Integer>, HashSet<String>>, Boolean>() {
+            @Override
+            public Boolean call(Tuple2<Tuple3<String, Integer, Integer>, HashSet<String>> tuple3HashSetTuple2) throws Exception {
+                Integer thisMonth = tuple3HashSetTuple2._1()._2();
+                Integer thisYear = tuple3HashSetTuple2._1()._3();
+
+                if (mostRecentStashCommitDate.get() != null) {
+                    Calendar cal = Calendar.getInstance();
+                    cal.setTime(mostRecentStashCommitDate.get());
+
+                    if (cal.get(Calendar.YEAR) > thisYear || (cal.get(Calendar.YEAR) == thisYear && cal.get(Calendar.MONTH) > thisMonth)) {
+                        //System.out.println("Filtering out these tuples for database! ");
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        }).map(new Function<Tuple2<Tuple3<String, Integer, Integer>, HashSet<String>>, StashUserCollaborationMetricInfoInternal>() {
+            @Override
+            public StashUserCollaborationMetricInfoInternal call(Tuple2<Tuple3<String, Integer, Integer>, HashSet<String>> tuple3HashSetTuple2) throws Exception {
+
+                Calendar cal = Calendar.getInstance();
+                cal.clear();
+                cal.set(Calendar.YEAR, tuple3HashSetTuple2._1()._3());
+                cal.set(Calendar.MONTH, tuple3HashSetTuple2._1()._2());
+
+                StashUserCollaborationMetricInfoInternal collabMetricInfo = new StashUserCollaborationMetricInfoInternal();
+                collabMetricInfo.setDeveloperId(tuple3HashSetTuple2._1()._1());
+                collabMetricInfo.setStashCollaborationCount(tuple3HashSetTuple2._2().size());
+                collabMetricInfo.setMetricMonthDate(cal.getTime());
+
+                System.out.println("STASH COLLABORATION STREAM: " + collabMetricInfo.getDeveloperId() + " " + collabMetricInfo
+                        .getStashCollaborationCount() + " " + collabMetricInfo.getMetricMonthDate());
+                return collabMetricInfo;
+
+            }
+        });
+
+        collabMetricsDStream.print();
+        javaFunctions(collabMetricsDStream).writerBuilder( keyspaceName, stashCollabMetricTlbName, mapToRow(StashUserCollaborationMetricInfoInternal.class) ).saveToCassandra();
+    }
+
+    public static void computeStashProductivityMetric(final JavaDStream<StashMetricIssue> stashIssuesDStream)
+    {
+        //Compute multiple projects collaboration metric
+        computeStashCollabMetric(stashIssuesDStream);
+
         //author,month,year - cntCommits,month, year
         JavaPairDStream<Tuple3<String, Integer, Integer>, Tuple3<Integer,Integer,Integer>> stashUserMonthlyProductivityMetrics = stashIssuesDStream.filter(new Function<StashMetricIssue, Boolean>() {
             @Override
@@ -99,11 +218,12 @@ public class StashStreamFunctions {
                 cal.setTime(date);
                 Integer month = cal.get(Calendar.MONTH);
                 Integer year = cal.get(Calendar.YEAR);
+                String author = stashMetricIssue.getAuthor().getName();
 
                 System.out.println(stashMetricIssue.getAuthor().getName() + " - " + stashMetricIssue.getAuthor().getDisplayName() + " - "
                         + stashMetricIssue.getAuthor().getEmailAddress() + stashMetricIssue.getAuthor().getId() + "\n\n\n");
 
-                return new Tuple2<Tuple3<String, Integer, Integer>, Tuple3<Integer, Integer, Integer>>(new Tuple3<String, Integer, Integer>(stashMetricIssue.getAuthor().getName(), month, year),
+                return new Tuple2<Tuple3<String, Integer, Integer>, Tuple3<Integer, Integer, Integer>>(new Tuple3<String, Integer, Integer>(author, month, year),
                         new Tuple3<>(1, month, year));
             }
         }).updateStateByKey(STASH_COMMIT_RUNNING_SUM);
@@ -143,7 +263,7 @@ public class StashStreamFunctions {
             }
         });
 
-        JavaDStream<StashUserMetricInfo.StashUserMetricInfoInternal> metricsDStream = stashUserMonthlyProductivityMetrics.filter(
+        JavaDStream<StashUserMetricInfoInternal> metricsDStream = stashUserMonthlyProductivityMetrics.filter(
                 new Function<Tuple2<Tuple3<String, Integer, Integer>, Tuple3<Integer, Integer, Integer>>, Boolean>()
 
                 {
@@ -168,11 +288,11 @@ public class StashStreamFunctions {
                         return false;
                     }
                 }).map(
-                new Function<Tuple2<Tuple3<String, Integer, Integer>, Tuple3<Integer, Integer, Integer>>, StashUserMetricInfo.StashUserMetricInfoInternal>()
+                new Function<Tuple2<Tuple3<String, Integer, Integer>, Tuple3<Integer, Integer, Integer>>, StashUserMetricInfoInternal>()
 
                 {
                     @Override
-                    public StashUserMetricInfo.StashUserMetricInfoInternal call(
+                    public StashUserMetricInfoInternal call(
                             final Tuple2<Tuple3<String, Integer, Integer>, Tuple3<Integer, Integer, Integer>> tuple3Tuple3Tuple2)
                             throws Exception {
 
@@ -181,7 +301,7 @@ public class StashStreamFunctions {
                         cal.set(Calendar.YEAR, tuple3Tuple3Tuple2._1()._3());
                         cal.set(Calendar.MONTH, tuple3Tuple3Tuple2._1()._2());
 
-                        StashUserMetricInfo.StashUserMetricInfoInternal metricInfo = new StashUserMetricInfo.StashUserMetricInfoInternal();
+                        StashUserMetricInfoInternal metricInfo = new StashUserMetricInfoInternal();
                         metricInfo.setDeveloperId(tuple3Tuple3Tuple2._1()._1());
                         metricInfo.setStashCommitCnt(tuple3Tuple3Tuple2._2()._1());
                         metricInfo.setMetricMonthDate(cal.getTime());
@@ -193,7 +313,7 @@ public class StashStreamFunctions {
                 });
 
         metricsDStream.print();
-        javaFunctions(metricsDStream).writerBuilder( keyspaceName, stashMetricTlbName, mapToRow(StashUserMetricInfo.StashUserMetricInfoInternal.class) ).saveToCassandra();
+        javaFunctions(metricsDStream).writerBuilder( keyspaceName, stashMetricTlbName, mapToRow(StashUserMetricInfoInternal.class) ).saveToCassandra();
     }
 
     public static void startStashStreaming(JavaStreamingContext jssc, HashSet<String> topicsSet, HashMap<String,
