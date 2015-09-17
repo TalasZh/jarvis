70e39ae0cae7856657b1e34196a342b4208334e4
Kazim Isik <kisik@critical-factor.com>
2015-09-17 18:25:22 +0300

    KMS-202

---
+++ src/main/java/org/safehaus/service/impl/UserMetricServiceImpl.java
@@ -0,0 +1,63 @@
+package org.safehaus.service.impl;
+
+import org.safehaus.analysis.UserMetricInfo;
+import org.safehaus.dao.Dao;
+import org.safehaus.service.UserMetricService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by kisik on 17.09.2015.
+ */
+@Service
+public class UserMetricServiceImpl implements UserMetricService {
+
+    private static final Logger log = LoggerFactory.getLogger(UserMetricServiceImpl.class);
+    @Autowired
+    private Dao dao;
+
+
+    @Override
+    public List<UserMetricInfo> getUserMetricInfoListByDevId(String developerId) {
+        log.info( "Getting UserMetricInfo list with developerID: {}", developerId );
+        String query =
+                "Select j from " + UserMetricInfo.class.getSimpleName() + " j where j.developerMonthInfo.developerId= " + developerId;
+        return (List<UserMetricInfo>) dao.findByQuery(query);
+    }
+
+    @Override
+    public UserMetricInfo getUserMetricInfo(String developerId, long timestamp) {
+        log.info( "Getting UserMetricInfo list");
+        String query =
+                "Select j from " + UserMetricInfo.class.getSimpleName() + " j where j.developerMonthInfo.developerId= "
+                        + developerId+" and j.developerMonthInfo.metricMonthTimestamp= " + timestamp;
+        List<UserMetricInfo> returnList = (List<UserMetricInfo>) dao.findByQuery(query);
+        if(returnList.size() == 1)
+            return returnList.get(0);
+        else {
+            return null;
+        }
+    }
+
+    @Override
+    public List<UserMetricInfo> getUserMetricInfoListByMonth(long timestamp) {
+        log.info( "Getting stashUserMetricInfo list with metricMonthDate: {}", timestamp);
+        String query =
+                "Select j from " + UserMetricInfo.class.getSimpleName() + " j where j.developerMonthInfo.metricMonthTimestamp= " + timestamp;
+        return (List<UserMetricInfo>) dao.findByQuery(query);
+    }
+
+
+    public Dao getDao()
+    {
+        return dao;
+    }
+    public void setDao( Dao dao )
+    {
+        this.dao = dao;
+    }
+}
