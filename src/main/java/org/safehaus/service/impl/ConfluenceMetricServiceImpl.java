c98bc7fc069bcd595b86f0fb1bf64c1cdaf891c6
Kazim Isik <kisik@critical-factor.com>
2015-09-17 19:45:00 +0300

    KMS-202

---
+++ src/main/java/org/safehaus/service/impl/ConfluenceMetricServiceImpl.java
@@ -0,0 +1,61 @@
+package org.safehaus.service.impl;
+
+import org.safehaus.analysis.ConfluenceMetricInfo;
+import org.safehaus.dao.Dao;
+import org.safehaus.service.ConfluenceMetricService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * Created by kisik on 17.09.2015.
+ */
+public class ConfluenceMetricServiceImpl implements ConfluenceMetricService {
+
+    private static final Logger log = LoggerFactory.getLogger(ConfluenceMetricServiceImpl.class);
+
+    @Autowired
+    private Dao dao;
+
+    @Override
+    public List<ConfluenceMetricInfo> getConfluenceMetricInfoListByDevId(String developerId) {
+        log.info("Getting ConfluenceMetricInfo list with developerID: {}", developerId);
+        String query =
+                "Select j from " + ConfluenceMetricInfo.class.getSimpleName() + " j where j.developerMonthInfo.developerId= " + developerId;
+        return (List<ConfluenceMetricInfo>) dao.findByQuery(query);
+    }
+
+    @Override
+    public ConfluenceMetricInfo getConfluenceMetricInfo(String developerId, long timestamp) {
+        log.info( "Getting ConfluenceMetricInfo list");
+        String query =
+                "Select j from " + ConfluenceMetricInfo.class.getSimpleName() + " j where j.developerMonthInfo.developerId= "
+                        + developerId+" and j.developerMonthInfo.metricMonthTimestamp= " + timestamp;
+        List<ConfluenceMetricInfo> returnList = (List<ConfluenceMetricInfo>) dao.findByQuery(query);
+        if(returnList.size() == 1)
+            return returnList.get(0);
+        else {
+            return null;
+        }
+    }
+
+    @Override
+    public List<ConfluenceMetricInfo> getConfluenceMetricInfoListByMonth(long timestamp) {
+        log.info( "Getting ConfluenceMetricInfo list with metricMonthDate: {}", timestamp);
+        String query =
+                "Select j from " + ConfluenceMetricInfo.class.getSimpleName() + " j where j.developerMonthInfo.metricMonthTimestamp= " + timestamp;
+        return (List<ConfluenceMetricInfo>) dao.findByQuery(query);
+    }
+
+    public Dao getDao()
+    {
+        return dao;
+    }
+
+    public void setDao( Dao dao )
+    {
+        this.dao = dao;
+    }
+}
